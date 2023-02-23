86ef69410a744971a5269d50be03254b1105cfa1
Tum Chatdanai <chatdanai2543@hotmail.com>
2023-02-23 22:26:06 +0700

    Update Home page slide

---
+++ lib/components/blog_slide.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlogSlide extends StatefulWidget {
+  final List<String> imageUrls;
+
+  BlogSlide({required this.imageUrls});
+
+  @override
+  _BlogSlideState createState() => _BlogSlideState();
+}
+
+class _BlogSlideState extends State<BlogSlide> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(viewportFraction: 0.8);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: widget.imageUrls.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Image.network(
+                widget.imageUrls[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:dental_clinic/learn.dart';
 import 'package:dental_clinic/components/homepage_function.dart';
-
+import 'package:dental_clinic/components/blog_slide.dart';
 import "package:flutter/material.dart";
 
 class Homepage extends StatelessWidget {
@@ -29,6 +29,17 @@ class Homepage extends StatelessWidget {
               ),
             ),
             HomeFunction(),
+            const SizedBox(
+              height: 30,
+            ),
+            BlogSlide(
+              imageUrls: [
+                'https://www.seraclinics.com/wp-content/uploads/2023/02/FD202.png',
+                'https://www.storynow.co/wp-content/uploads/2022/07/DMC-PR-News-02-628x628.jpg',
+                'https://www.rcskinclinic.com/getattachment/news-activities/%E0%B8%AA%E0%B8%B2%E0%B8%82%E0%B8%B2%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B9%80%E0%B8%9B%E0%B8%B4%E0%B8%94%E0%B9%83%E0%B8%AB%E0%B9%89%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%8A%E0%B9%88%E0%B8%A7%E0%B8%87%E0%B8%9B%E0%B8%B5%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-2019/news-clinic-close-New-year62-detail-(4).jpg.aspx',
+                'https://www.slcclinic.com/uploads/images/211224150018W474-940p.jpg',
+              ],
+            ),
           ],
         ),
       ),
